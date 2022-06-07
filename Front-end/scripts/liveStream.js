c754359bc43baf5a9682f45d99dfeedbacbc8e32
vonhuhung1 <65439591+vonhuhung1@users.noreply.github.com>
2022-06-07 17:06:39 +0700

    update livestream

--- Front-end/scripts/liveStream.js
+++ Front-end/scripts/liveStream.js
@@ -1,33 +1,23 @@
 const socket = io.connect('http://localhost:3000');
+const peer = new Peer();
 
-socket.on("Sever-send-roomLive",roomIdLive => {
-    console.log(roomIdLive);
-    document.getElementById("Room-id").innerHTML = roomIdLive;})
-        
-    let btn4 = document.getElementById('btn-sendDataLive')
-    btn4.addEventListener("click", () => {
-        roomSend = document.getElementById("txt_roomId").value;
-        socket.emit("Send-roomLive",roomSend);
-})
-    
-socket.on("Sever-send-dataLiveStart", data => {
-    console.log(data);
-    let video = document.querySelector("video");
-    video.src = data;
-})
 
-// socket.on("Sever-send-data-end"), data => {
-//     data = null ;
-// }
+// socket.on("Sever-send-roomLive",roomIdLive => {
+//     document.getElementById("Room-id").innerHTML = roomIdLive;
+// })      
+peer.on('open', function(id) {
+    document.getElementById("Room-id").innerHTML = id;
+});
 const video = document.querySelector("video");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
+
 
 var displayMediaOptions = {
     video: {
     cursor: "always", 
 },
-    audio: true,
+audio: false,
 };
 
 start.onclick = function (e) {
@@ -36,35 +26,38 @@ start.onclick = function (e) {
 stop.onclick = function (e) {
     stopSharing();
 };
-
-async function startSharing() {
-    try {
-    let stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+function displayScreen(stream){
     video.srcObject = stream;
-    const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9") 
-            ? "video/webm; codecs=vp9" 
-            : "video/webm"
-    let mediaRecorder = new MediaRecorder(stream, {
-        mimeType: mime
-    }) 
-    let chunks = []
-    mediaRecorder.addEventListener('dataavailable', function(e) {
-        chunks.push(e.data)
-    })
-        let blob = new Blob(chunks, {
-        type: chunks[0]
-    })
-        console.log(blob);
-        let url = URL.createObjectURL(blob);
-        console.log(url)
-        socket.emit("StartShare-Data",url);
-    } catch (error) {
-        console.log(error);
+    video.onloadedmetadata = () => {
+        video.play();
     }
 }
-function stopSharing() {
-    let tracks = video.srcObject.getTracks();
-    tracks.forEach(track => track.stop());
-    video.srcObject = null;
-    // socket.emit("StopShare-Data",false)
+
+var streams ;
+    async function startSharing() {
+        await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+            .then(stream => {
+                displayScreen(stream);
+                streams = stream;
+            })
+            .catch(error => {
+                console.log(error);
+        })
 }
+
+function stopSharing() {
+    displayScreen(null);
+}  
+    let btn4 = document.getElementById('btn-sendDataLive')
+    btn4.addEventListener("click", () => {
+        roomSend = document.getElementById("txt_roomId").value;
+        displayScreen(streams)
+        var call = peer.call(roomSend,streams)
+        call.on('stream', remoteStream => displayScreen(remoteStream))
+}) 
+
+peer.on("call", call => {
+    displayScreen(streams,video);
+    call.answer(streams);
+    call.on('stream', remoteStream => displayScreen(remoteStream,video))
+})
--- Front-end/scripts/record.js
+++ Front-end/scripts/record.js
@@ -19,7 +19,6 @@ btn.addEventListener("click", async function () {
     let stream = await navigator.mediaDevices.getDisplayMedia({
         video: true,
     })
-
     const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9") 
             ? "video/webm; codecs=vp9" 
             : "video/webm"
